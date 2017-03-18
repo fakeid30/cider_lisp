4cd89b7931aff398a1499fb436109f31d80d20aa
Fake ID <junayeed@protonmail.com>
2017-03-18 21:09:50 +0600

    Added basic parsing

--- main.c
+++ main.c
@@ -24,18 +24,47 @@ char* readline(char* prompt) {
 
 int main() {
 
+/* Create Some Parsers */
+    mpc_parser_t* Number   = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr     = mpc_new("expr");
+    mpc_parser_t* Lispy    = mpc_new("lispy");
+
+/* Define them with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+              "                                         \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+              Number, Operator, Expr, Lispy);
+
+
     puts("Lispy Version 0.0.0.0.1");
     puts("Press ctrl+c to exit");
 
     while (1) {
-        char* input = readline("lispy name> ");
+        char* input = readline("lispy > ");
 
         add_history(input);
 
-        printf("No you're not %s\n", input);
+        /* Attempt to Parse the user Input */
+        mpc_result_t r;
+
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            /* On Success Print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* Otherwise Print the Error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
 
+    mpc_cleanup(4,Number,Operator,Expr,Lispy);
     return 0;
 }
